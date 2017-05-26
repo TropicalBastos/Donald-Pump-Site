678c30d4826bf72721944a966a2d08f7ee26f6d9
TropicalBastos <ian-bastos@live.com>
2017-05-26 20:53:17 +0100

    Firefox fix

--- public/js/components/additional/Contact.js
+++ public/js/components/additional/Contact.js
@@ -7,10 +7,25 @@ class Contact extends React.Component{
     this.state = {stage:"button"};
   }
 
+  click(){
+    //display the about modal
+    var element = document.getElementById("contact-div");
+    var display = getComputedStyle(element, null).display;
+    if(display === "none" && MODAL === false){
+      element.style.display = "block";
+      setTimeout(()=>{element.classList.add("popin");},100);
+      MODAL = true;
+    }else{
+      element.classList.remove("popin");
+      setTimeout(()=>{element.style.display = "none";},500);
+      MODAL = false;
+    }
+  }
+
   render(){
     if(this.state.stage==="button"){
       return(
-        <div className="secondary-button">
+        <div onClick={this.click} className="secondary-button">
           <p>Contact</p>
         </div>
       );
