51e3fd35fa1f31a9a57599f158d5e0fd11d7d909
TropicalBastos <ian-bastos@live.com>
2017-11-26 23:03:08 +0000

    release update

--- public/js/components/HomeOrb.js
+++ public/js/components/HomeOrb.js
@@ -14,7 +14,7 @@ class HomeOrb extends React.Component{
               <div className="homedesc-container">
                 <p className="home-desc">Welcome to the official site for the Donald Pump mobile
                 app!</p>
-                <p className="home-desc">Soon to be available on iOS, Android and Windows phone in October 2017</p>
+                <p className="home-desc">Soon to be available on iOS, Android and Windows phone in December 2017</p>
               </div>
               <img src="res/plane.png" className="home-plane"/>
            </div>
