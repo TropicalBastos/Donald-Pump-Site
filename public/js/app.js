bc6d252c2e73d5b50d2b21a5f5f18bb722801d33
TropicalBastos <ian-bastos@live.com>
2018-02-18 01:20:14 +0000

    backend newsletter implemented

--- public/js/app.js
+++ public/js/app.js
@@ -6,13 +6,20 @@ import {Orb} from './components/Orb.js';
 import {AdditionalComponents} from './components/additional/AdditionalComponents.js';
 import {loadParticles} from './particles.js';
 
-runCanvas();
 loadParticles();
-//global var that is used by components to fire events from other components
-window.globalVar = {};
-//global bool that tests if a modal is already open
-window.MODAL = false;
 
-ReactDOM.render(<Orb />,document.getElementById("maincontentwrapper"));
-ReactDOM.render(<MainButtons />,document.getElementById("buttoncontainer"));
+try{
+    runCanvas();
+    //global var that is used by components to fire events from other components
+    window.globalVar = {};
+    //global bool that tests if a modal is already open
+    window.MODAL = false;
+
+    ReactDOM.render(<Orb />,document.getElementById("maincontentwrapper"));
+    ReactDOM.render(<MainButtons />,document.getElementById("buttoncontainer"));
+}catch(e){
+    //do nothing
+}
+
+//footer
 ReactDOM.render(<AdditionalComponents />,document.getElementById("footer"));
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var bodyParser = require("body-parser");
 var mongoClient = require("mongodb").MongoClient;
 var PORT = 8080;
 var app = express();
+const credentials = require("./credentials.json");
+const username = credentials.username;
+const password = credentials.password;
 
 //## Mongo connection settings ##################
 const url = "mongodb://localhost";
@@ -15,6 +18,7 @@ const subscriptions = "subscriptions";
 app.use(express.static(__dirname+"/public"));
 app.use(express.static(__dirname+"/node_modules/particles.js/"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 //###############################################
 
 
@@ -27,7 +31,36 @@ app.set("view engine","ejs");
 
 // ## Routes ####################################
 app.get("/",(req,res)=>{
-  res.render("index.html");
+  res.render("index");
+});
+
+app.get("/downloadEmails", (req, res) => {
+  message = {};
+  if(req.query.invalid)
+    message = {error: "Invalid username or password"};
+  res.render("downloademails", message);
+});
+
+app.post("/emails.csv", (req, res) => {
+  if(req.body.user === username
+     && req.body.pass === password){
+       mongoClient.connect(url, (error, client) => {
+        const db = client.db(dbName);
+        db.collection(subscriptions).find().toArray((error, result) => {
+          var emails= [];
+          for(var r in result){
+            emails.push(result[r].email);
+          }
+          var csv = toCsv(emails);
+          res.setHeader("Content-Type", "application/csv");
+          res.setHeader("filename", "emails.csv");
+          res.send(csv);
+          client.close();
+        });
+       });
+     }else{
+       res.redirect(301, "/downloadEmails?invalid=yes");
+     }
 });
 
 app.post("/subscribe", (req, res) => {
@@ -55,6 +88,7 @@ app.post("/subscribe", (req, res) => {
           console.log(email + " inserted succesfully into db");
         }
         res.send({message: "Thank you!"});
+        client.close();
       });
     });
   });
@@ -67,10 +101,15 @@ app.listen(PORT,()=>{
 //#################################################
 
 //## Helpers ######################################
-
 function validateEmail(email){
   var emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   return email.match(emailPattern);
 }
-
+function toCsv(array){
+  csv = "";
+  for(item in array){
+    csv += array[item] + ",";
+  }
+  return csv.substring(0, csv.length - 1);
+}
 //#################################################
